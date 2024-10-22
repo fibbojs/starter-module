71837cdea2c8972a5d318a7651e10c2c53dc9185
Gugustinette <mercier.augustin@outlook.fr>
2024-10-23 01:04:08 +0200

    chore: update to v0.0.10

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,5 +1,4 @@
-import RAPIER from '@dimforge/rapier3d'
-import { FAmbientLight, FComponentEmpty, FCuboid, FDirectionalLight, FOrbitCamera, FScene } from '@fibbojs/3d'
+import { FAmbientLight, FComponentEmpty, FCuboid, FDirectionalLight, FOrbitCamera, FRigidBodyType, FScene } from '@fibbojs/3d'
 import { fDebug } from '@fibbojs/devtools'
 import './style.css'
 import { CustomController } from '../../src'
@@ -12,20 +11,21 @@ import { CustomController } from '../../src'
   scene.init()
   await scene.initPhysics()
   // Debug the scene
+  // @ts-ignore
   if (import.meta.env.DEV)
     fDebug(scene)
 
   // Add directional light to represent the sun
-  scene.addLight(new FDirectionalLight(scene, {
+  new FDirectionalLight(scene, {
     position: { x: 20, y: 20, z: 0 },
     color: 0xFFFFFF,
     intensity: 2,
-  }))
+  })
   // Add ambient light
-  scene.addLight(new FAmbientLight(scene, {
+  new FAmbientLight(scene, {
     color: 0x404040,
     intensity: 20,
-  }))
+  })
 
   // Create a ground
   const ground = new FCuboid(scene, {
@@ -33,7 +33,7 @@ import { CustomController } from '../../src'
     scale: { x: 15, y: 0.1, z: 15 },
   })
   ground.initRigidBody({
-    rigidBodyType: RAPIER.RigidBodyType.Fixed,
+    rigidBodyType: FRigidBodyType.FIXED,
   })
   ground.setColor(0x348C31)
   scene.addComponent(ground)
@@ -44,13 +44,13 @@ import { CustomController } from '../../src'
     scale: { x: 1, y: 1, z: 1 },
   })
   cube.initRigidBody({
-    rigidBodyType: RAPIER.RigidBodyType.Fixed,
+    rigidBodyType: FRigidBodyType.FIXED,
   })
   cube.controller = new CustomController({ component: cube })
   scene.addComponent(cube)
 
   // Add orbital camera controls
-  scene.camera = new FOrbitCamera({
+  scene.camera = new FOrbitCamera(scene, {
     target: new FComponentEmpty(scene),
   })
 })()
--- src/CustomController.ts
+++ src/CustomController.ts
@@ -9,14 +9,8 @@ export class CustomController extends FControllerCore {
     super(options)
   }
 
-  /**
-   * @description Update the controller. Should be called every frame.
-   * The purpose of `onFrame` on FController is to update the component's transform in the desired way.
-   * Any rendering process should be done on the component, not here.
-   * @param delta The time since the last frame.
-   */
-  onFrame(delta: number): void {
+  frame(delta: number): void {
     // Rotate the component by 1 degree every frame
-    this.component.rotationDegreeX += 1
+    this.component.transform.rotationDegreeX += 30 * delta
   }
 }
